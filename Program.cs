50df7acbba4a97e30b783822c21b02ba4e1e9c87
Vlad <60320358@mail.ru>
2022-11-28 23:29:53 +0300

    Готово!!!

--- Program.cs
+++ Program.cs
@@ -7,3 +7,75 @@ Console.WriteLine("Hello, World!");
 на стирте выполнения алгоритма, При решении не рекомендуется пользоваться коллекциями, 
 пучше обойтись исключительно массивами.
  */
+
+
+int count = 4;
+string[] arrya = new string[count];
+int length_arrya = 3;
+
+
+string GetArray()
+{
+    Console.WriteLine("Введите элемент массива:");
+    string array = Console.ReadLine();
+    return array;
+}
+
+
+// Met_GetArray метод получил масив
+string[] MetGetArray()
+{
+    for (int i = 0; i < count; i++)
+    {
+        arrya[i] = GetArray();
+    }
+    return arrya;
+}
+
+string[] array = MetGetArray();
+
+// met_new_array метод сравнение и получения нового масива
+int MetСomparisonArray()
+{    
+    int count2 = 0;
+    for (int j = 0; j < count; j++)
+    {
+        if (length_arrya <= array[j].Length)
+        {
+            count2++;
+        }
+    }
+    return count2;
+}
+
+string[] MetNewArray()
+{
+    string[] arrya_end = new string[MetСomparisonArray()];
+    int count3 = 0;
+
+    for (int j = 0; j < count; j++)
+    {
+        if (length_arrya <= array[j].Length)
+        {
+            arrya_end[count3] = array[j];
+            count3++;
+        }
+    }
+    return arrya_end;
+}
+
+ string[] array4 = MetNewArray();
+
+
+// Show_console() метод показывает результат;
+Console.Write("Новый массив = ");
+for (int i = 0; i < array4.Length; i++)
+{
+    if (i+1 < array4.Length)
+    {
+        Console.Write(array4[i]+", ");
+    }
+
+    else { Console.Write(array4[i]);}
+    
+}
